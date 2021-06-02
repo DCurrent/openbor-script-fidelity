3603204842336b9f7f05cf1f15c95e36c69dd6c8
Damon V. Caskey <dc@caskeys.com>
2021-06-02 12:08:16 -0400

    Update global.c

--- global.c
+++ global.c
@@ -1,8 +1,7 @@
 #include "data/scripts/dc_fidelity/config.h"
 
-// Try global category when specific category doesn't have sound type.
+/* Try global category when specific category doesn't have sound type. */
 
-// Get
 void dc_fidelity_get_member_global_fallback()
 {
 	char id;
@@ -21,18 +20,19 @@ void dc_fidelity_get_member_global_fallback()
 	return result;
 }
 
-// Set
 void dc_fidelity_set_member_global_fallback(void value)
 {
 	char id;
 
-	// Get id key.
+	/* Get id key. */
 	id = dc_fidelity_get_instance() + DC_FIDELITY_MEMBER_GLOBAL_FALLBACK;
 
-	// If the value is same as default, then use
-	// make sure variable is deleted instead. We fall
-	// back to default for empty vars, so may as
-	// well save the memory.
+	/*
+	* If the value is same as default, then 
+	* make sure variable is deleted instead. 
+	* We fall back to default for empty vars, 
+	* so may as well save the memory.
+	*/
 	if (value == DC_FIDELITY_DEFAULT_GLOBAL_FALLBACK)
 	{
 		value = NULL();
