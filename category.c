6902909722d9a74c86f413889f24b41323be8b73
Damon V. Caskey <dc@caskeys.com>
2021-06-02 12:10:27 -0400

    Update category.c
    Clean up documentation.

--- category.c
+++ category.c
@@ -1,12 +1,15 @@
 #include "data/scripts/dc_fidelity/config.h"
 
-// Get
+/* 
+* Category is the base level for seeking a sound. 
+* Normally it is a model name, but can be anything.
+*/
+
 void dc_fidelity_get_member_category_list()
 {
 	char id;
 	void result;
 
-	// Get id key.
 	id = dc_fidelity_get_instance() + DC_FIDELITY_MEMBER_SOUND_CATEGORY;
 
 	result = getglobalvar(id);
@@ -19,18 +22,12 @@ void dc_fidelity_get_member_category_list()
 	return result;
 }
 
-// Set
 void dc_fidelity_set_member_category_list(void value)
 {
 	char id;
 
-	// Get id key.
 	id = dc_fidelity_get_instance() + DC_FIDELITY_MEMBER_SOUND_CATEGORY;
 
-	// If the value is same as default, then use
-	// make sure variable is deleted instead. We fall
-	// back to default for empty vars, so may as
-	// well save the memory.
 	if (value == DC_FIDELITY_DEFAULT_CATEGORY_LIST)
 	{
 		value = NULL();
