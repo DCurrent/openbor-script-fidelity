ac1feda81845963a69ee8345ebb724e25d6e29c1
Damon V. Caskey <dc@caskeys.com>
2021-06-02 12:08:52 -0400

    Update global.c
    Clean up documentation.

--- global.c
+++ global.c
@@ -7,7 +7,6 @@ void dc_fidelity_get_member_global_fallback()
 	char id;
 	void result;
 
-	// Get id key.
 	id = dc_fidelity_get_instance() + DC_FIDELITY_MEMBER_GLOBAL_FALLBACK;
 
 	result = getlocalvar(id);
@@ -24,15 +23,8 @@ void dc_fidelity_set_member_global_fallback(void value)
 {
 	char id;
 
-	/* Get id key. */
 	id = dc_fidelity_get_instance() + DC_FIDELITY_MEMBER_GLOBAL_FALLBACK;
 
-	/*
-	* If the value is same as default, then 
-	* make sure variable is deleted instead. 
-	* We fall back to default for empty vars, 
-	* so may as well save the memory.
-	*/
 	if (value == DC_FIDELITY_DEFAULT_GLOBAL_FALLBACK)
 	{
 		value = NULL();
